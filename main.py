7cc8bdc55d34798cc69ff02421cb6fab490f14bc
Ross Callaghan <ross.callaghan.16@ucl.ac.uk>
2022-04-11 11:44:31 +0100

    intial implementation of the main function

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import mlflow
+import submod_for_mlflow_test as sm
+
+
+def main():
+
+    print('Running the mlflow test')
+    print('Running a submodule function ')
+    sm.print_statement()
+
+    print('Mlflow worked')
+    mlflow.log_metric('worked', 1)
+
+if __name__ == "__main__":
+    main()
