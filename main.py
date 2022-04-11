e652e5c0e54a5272f6f3ac2e964745808ff9e1ae
Ross Callaghan <ross.callaghan.16@ucl.ac.uk>
2022-04-11 11:58:24 +0100

    Print the commit

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import mlflow
 import submod_for_mlflow_test.submod as sm
 
 
-
 def main():
 
     print('Running the mlflow test')
     print('Running a submodule function ')
     sm.print_statement()
 
-    print('Mlflow worked')
+    run = mlflow.active_run()
+    git_commit = run.data.tags['mlflow.source.git.commit']
+
+    print('Mlflow worked on commit {}'.format(git_commit))
     mlflow.log_metric('worked', 1)
+    mlflow.log_param('git_commit', git_commit)
 
 if __name__ == "__main__":
     main()
